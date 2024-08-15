c5acbdd27621017a5849249dabc3ad9263a8723e
webdevk28 <webdevk28@gmail.com>
2024-08-15 12:43:09 +0700

    update commit

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-function handleSubmit() {
-const selectedOption = document.querySelector('input[name="market-info"]:checked');
-if (selectedOption) {
-    window.location.href = selectedOption.value;
-}
-}
-
 let showform = document.getElementById("showform").style
 const handleShowform = ()=>{
 showform.display="block";
