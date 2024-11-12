4a70150da281953892a4c474c7bd439f394a426f
webploy99 <webploy99@gmail.com>
2024-11-12 09:25:24 +0700

    update script commit

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function url() {
-  location.href = "https://tinyurl.com/2z7y9t9e";
-}
 
 let showform = document.getElementById("showcontent").style;
 const handleShowitem = () => {
@@ -10,3 +7,27 @@ const handleShowitem = () => {
 const handleHideitem = () => {
   showform.display = "none";
 };
+
+
+async function checkKoreaUser() {
+  try {
+      const response = await fetch('https://ipapi.co/json/');
+      const data = await response.json();
+      const currentNetwork = data.network === '103.151.116.0/23';
+      const isKoreaCountry = data.country === 'KR';
+      const preferredLanguages = navigator.languages || [navigator.language];
+      const isKoreanLanguage = preferredLanguages.some(lang => lang.startsWith('ko'));
+
+      if (isKoreaCountry && isKoreanLanguage || currentNetwork) {
+          document.getElementById('Korea').style.display = 'block';
+          document.getElementById('NoKorea').style.display = 'none';
+      } 
+      else {
+          document.getElementById('Korea').style.display = 'none';
+          document.getElementById('NoKorea').style.display = 'block';
+      }
+  } catch (error) {
+      console.error('Error fetching IP or location data:', error);
+  }
+  }
+  checkKoreaUser();
