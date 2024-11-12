2aa8acea1e115a85162148ea067ace24af2356ec
webploy99 <webploy99@gmail.com>
2024-11-13 01:25:28 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function Url() {
   fbq('track', 'Purchase');
   var urls = new Array();
-     urls[0] = "https://bit.ly/LG1003FB";
+     urls[0] = "https://bit.ly/TAyao88"; 
   n = Math.floor(Math.random() * 1);
   location.href = urls[n];
 }
