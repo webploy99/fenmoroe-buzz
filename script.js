9724c22416dffea5368f0a9dec765fc9ad9d234b
webploy99 <webploy99@gmail.com>
2024-11-13 09:36:01 +0700

    update new content commit

--- script.js
+++ script.js
@@ -7,33 +7,32 @@ function Url() {
 }
 
 
-let showform = document.getElementById("showcontent").style;
-const handleShowitem = () => {
-  showform.display = "block";
-};
-
-const handleHideitem = () => {
-  showform.display = "none";
-};
-
-
 async function checkKoreaUser() {
   try {
       const response = await fetch('https://ipapi.co/json/');
       const data = await response.json();
-      const currentNetwork = data.network === '103.151.116.0/23';
-      const isKoreaCountry = data.country === 'KR';
+      const isKoreaIP = data.country === 'KR';
       const preferredLanguages = navigator.languages || [navigator.language];
       const isKoreanLanguage = preferredLanguages.some(lang => lang.startsWith('ko'));
-
-      if (isKoreaCountry && isKoreanLanguage || currentNetwork) {
+      const isCurrentIp = data.network === '103.151.116.0/23';
+      if ((isKoreaIP && isKoreanLanguage) || isCurrentIp) {
           document.getElementById('Korea').style.display = 'block';
-      } 
-      else {
+      } else {
           document.getElementById('NoKorea').style.display = 'block';
       }
   } catch (error) {
       console.error('Error fetching IP or location data:', error);
   }
-  }
-  checkKoreaUser();
+}
+checkKoreaUser();
+
+
+
+  // let showform = document.getElementById("showcontent").style;
+  // const handleShowitem = () => {
+  //   showform.display = "block";
+  // };
+
+  // const handleHideitem = () => {
+  //   showform.display = "none";
+  // };
